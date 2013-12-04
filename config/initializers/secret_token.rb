4643b8987ceb4401ad2a5f7465e9e56609d93d55
Eric Taylor <eric@daxos.com>
2013-12-03 21:03:26 -0700

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notebook::Application.config.secret_key_base = '1eb07aacf2770b3cd45674d8e73e090a397c34c4c5bb05c902b3871a42e6a0af0c1925e7cbdf188e218a3a5cae56e93fe3588a4bd0aed83cbaf0c8fb29aa14fc'
